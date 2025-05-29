1701704f82e40ddb2f39cb913b431a4c29ebf2b0
pi <stathisvotsis@gmail.com>
2025-05-29 20:57:52 +0300

    ready

--- ina260/ina260.py
+++ ina260/ina260.py
@@ -2,17 +2,14 @@
 import rclpy
 from rclpy.node import Node
 from std_msgs.msg import String
-#import time
-#import board
-#import adafruit_ina260
+import time
+import board
+import adafruit_ina260
+import busio
 
-
-#i2c = board.I2C()
-#ina260 = adafruit_ina260.INA260(i2c)
-#while True:
-    #print("Current: %.2f Voltage: %.2f Power:%.2f"
-        #%(ina260.current, ina260.voltage, ina260.power))
-    #time.sleep(1)
+#print("Current:", ina260.current)
+#print("Voltage:", ina260.voltage)
+#print("Power:", ina260.power)
 
 class Ina260Publisher(Node):
 
@@ -21,26 +18,21 @@ class Ina260Publisher(Node):
         self.publisher_ = self.create_publisher(String, 'voltage', 10)
         timer_period = 10  # seconds
         self.timer = self.create_timer(timer_period, self.timer_callback)
-        self.i = 0
+        i2c = busio.I2C(board.SCL, board.SDA)
+        self.ina260 = adafruit_ina260.INA260(i2c)
 
     def timer_callback(self):
         msg = String()
-        msg.data = 'Hello World: %d' % self.i
+        msg2 = round(self.ina260.voltage, 2)
+        msg.data = str(msg2)
         self.publisher_.publish(msg)
-        self.get_logger().info('Publishing: "%s"' % msg.data)
-        self.i += 1
+        #self.get_logger().info('Publishing: "%s"' % msg.data)
 
 
 def main(args=None):
     rclpy.init(args=args)
-
     ina260_publisher = Ina260Publisher()
-
     rclpy.spin(ina260_publisher)
-
-    # Destroy the node explicitly
-    # (optional - otherwise it will be done automatically
-    # when the garbage collector destroys the node object)
     ina260_publisher.destroy_node()
     rclpy.shutdown()
 
